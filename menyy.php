6eadbc8f41251f5576fc1bd3b9ecbc4d165a545c
Gert Adamson <gert.adamson@tigu.hk.tlu.ee>
2015-04-02 16:11:30 +0300

    test

--- menyy.php
+++ menyy.php
@@ -1,19 +1,20 @@
 <!doctype html>
 <html>
   <head>
-    <title>Menüü proov</title>
+    <title>MenÃ¼Ã¼ proov - muutus veebi kaudu</title>
   </head>
   <body>
      <select name="maakonnad">
 	   <option>Hiiumaa</option>
-	   <option selected="selected">Läänemaa</option>
+	   <option selected="selected">LÃ¤Ã¤nemaa</option>
 	   <option>Harjumaa</option>
 	 </select>
 	 <form action="?">
 	  <select name="riigivalik">
 	 <?php 
-	   $riigid=array("Soome", "Eesti", "Läti", "Leedu");
+	   $riigid=array("Soome", "Eesti", "LÃ¤ti", "Leedu");
 	   $valikunr=2;
+	   if(isSet($_REQUEST["riigivalik"])){$valikunr=intval($_REQUEST["riigivalik"]);}
 	   for($riiginr=0; $riiginr<count($riigid); $riiginr++){
 	      $lisand=($riiginr==$valikunr) ? "selected='selected'" : "";
 	      echo "<option value='$riiginr' $lisand>$riigid[$riiginr]</option>\n";
